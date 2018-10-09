8e3c41322389171b8c193ad35cdb2bdbc7fe008a
Amrendra Singh <ashking13th@gmail.com>
2018-10-09 11:05:44 +0530

    process dataset improved

--- assignments/assign-2/imageToColorHistogram.py
+++ assignments/assign-2/imageToColorHistogram.py
@@ -3,19 +3,33 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import os
 
-class imageToColorHistogram:
+class imageToHistogram:
     imagePath = "/"
     targetPath = "/"
     image = 0
+    featureVectors = []
 
-    def __init__(self, imageLoc, outputFolder):
-        imagePath = imageLoc
-        targetPath = outputFolder+"/"+imageLoc
-        image = mpimg.imread(imagePath)
-        print(image)
+    def __init__(self, source, imageName, path, outputFolder):
+        imagePath = path
+        targetPath = outputFolder+"/"+path
+        #print("Image name: ",imageName)
+        try:
+            #print("CAT 1")
+            image = mpimg.imread(imagePath)
+            self.process()
+            #print(image)
+            # print(len(image)," : ", len(image[0]))
+            # if(len(image) % 32 != 0 or len(image[0]) % 32 != 0):
+            #     print("NOT SCALED")
+        except IOError:
+            print("Not an Image File")
 
-    # def processPath():
-    #     pass
+    def processPatch(self, patch):
+        if(len(patch) )
 
-    # def processImage():
-    #     pass
+    def processImage(self):
+        pass
+
+    def process(self):
+
+        pass
--- assignments/assign-2/prepareData.py
+++ assignments/assign-2/prepareData.py
@@ -1,9 +1,15 @@
 import numpy as np
 import os
 import argparse
+from imageToColorHistogram import imageToHistogram as toHisto
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--source", required=True, help="Raw data set location")
 ap.add_argument("-d", "--dest", required=True, help="Output data set location")
 args = vars(ap.parse_args())
+
+for root, dirs, files in os.walk(args["source"]):
+    for f in files:
+        print(os.path.relpath(os.path.join(root, f), "."))
+        dataset = toHisto(args["source"], f, os.path.relpath(os.path.join(root, f), "."), args["dest"])
