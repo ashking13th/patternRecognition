cc4910008e264bc9c358455418c127c64387fc5a
Amrendra Singh <ashking13th@gmail.com>
2018-10-16 19:00:53 +0530

    gmm functions

--- assignments/assign-2/scripts/gmm.py
+++ assignments/assign-2/scripts/gmm.py
@@ -3,6 +3,15 @@ import os
 import matplotlib
 
 
+#   Calcualte gaussian function value for some x,mean and covMat
+def gaussian(covMat, x, mean):
+    gaussian = -(1/2)*(x-mean)*(np.linalg.inv(covMat))*(x-mean)
+    gaussian = np.exp(gaussian)
+    gaussian /= 1/(np.sqrt((2*np.pi)*covMat))
+    return gaussian
+
+
+# Finding covariance matrix for Kth cluster
 def updateCovMatK(gammaK, X, meanK, noOfPoints, dimensions):
     sigma = np.zeros(shape=(dimensions, dimensions))
     gammaSum = 0
@@ -16,7 +25,7 @@ def updateCovMatK(gammaK, X, meanK, noOfPoints, dimensions):
                 sigma[i][j] = 0
     return sigma
 
-
+#   Updating the entire covariance matrix vector using updateCovMatK() on K elements
 def updateCovMatVector(noOfPoints, X, meanVect, gammaVect, dimensions, clusters):
     covMatVect = []
     for k in range(clusters):
@@ -24,29 +33,55 @@ def updateCovMatVector(noOfPoints, X, meanVect, gammaVect, dimensions, clusters)
     return covMatVect
 
 
-def updateMuK(covMat):
-    pass
+def updateMeanK(gammaK, X, noOfPoints, dimensions):
+    mean = np.zeros(shape=(dimensions))
+    gammaSum = 0
+    for n in range(noOfPoints):
+        mean += gammaK[n]*X[n]
+        gammaSum += gammaK[n]
+    mean = mean/gammaSum
+    return mean  
 
 
-def updateMuVect():
-    pass
+def updateMeanVect(noOfPoints, X, gammaVect, dimensions, clusters):
+    meanVect = []
+    for k in range(clusters):
+        meanVect.append(updateMeanK(gammaVect[k], X, noOfPoints, dimensions))
+    return meanVect
 
 
-def updatePiK():
-    pass
+def updatePiK(gammaK, noOfPoints):
+    piK = 0
+    for n in range(noOfPoints):
+        piK += gammaK[n]
+    piK /= noOfPoints
+    return piK
 
 
-def updatePiVect():
-    pass
+def updatePiVect(noOfPoints, gammaVect, clusters):
+    piVect = []
+    for k in range(clusters):
+        piVect.append(updatePiK(gammaVect[k], noOfPoints))
+    return piVect
 
 
-def updateGammaNK():
-    pass
+def updateGammaNK(k, piK, x, covMatVect, meanVect, clusters):
+    gamma = 0
+    for ik in range(clusters):
+        gaussian(covMatVect[ik], x, meanVect[ik])
+    gamma /= gaussian(covMatVect[k], x, meanVect[k])
+    return gamma
 
 
-def updateGammaVect():
-    pass
-
+def updateGammaVect(piVect, noOfPoints, X, covMatVect, meanVect, clusters):
+    gammaVect = []
+    
+    for k in range(clusters):
+        gammaRow = []
+        for n in range(noOfPoints):
+            gammaRow.append(updateGammaNK(k, piVect[k], X[n], covMatVect[k], meanVect[k], clusters))
+        gammaVect.append(gammaRow)
+    return gammaVect
 
 def updateParameterVectors():
     pass
---
+++ assignments/kmeans.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import argparse
+import math
+import random
+
+''' 
+	Select k distinct random vectors from the features dataset for each (class(combining all images data))
+	Now, for each image we want to have a k(32) dimensional bag of visual words
+'''
+
+numOfClusters = 32
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--source", required=True, help="Raw data set location")
+args = vars(ap.parse_args())
+
+cnt = 0
+wholeData = []
+lengthOfFile = []
+
+
+def fileHandle(fileName):
+	file = open(fileName)
+	tempList = []
+
+	for line in file:
+		teLine = line.rstrip('\n ').split(' ')
+		nLine = [float(i) for i in teLine]
+		wholeData.append(nLine)
+
+	file.close()
+	return
+
+
+def euclidDist(centroid, dataPt):
+	centroid = np.array(centroid)
+	dataPt = np.array(dataPt)
+	ldist = centroid-dataPt
+	return np.sum(np.transpose(ldist)*ldist)
+
+
+def distArray(dataPt):
+	distVector = np.zeros((numOfClusters))
+	for ind in range(numOfClusters):
+		distVector[ind] = euclidDist(meanVector[ind], dataPt)
+	return distVector
+
+#assignment of clusters
+
+
+def assignDataPt():
+	costFunc = 0.0
+	for dataPt in wholeData:
+		distVector = distArray(dataPt)
+		noCl = np.argmin(distVector)
+		clusters[noCl].append(dataPt)
+		costFunc += distVector[noCl]
+	return costFunc
+
+
+def findMean(component):
+	total = np.zeros((len(component[0])))
+	for point in component:
+		total += np.array(point)
+	total /= len(component)
+	return total
+
+
+def reCalcMean():
+	for i in range(numOfClusters):
+		meanVector[i] = findMean(clusters[i])
+
+
+for root, dirs, files in os.walk(args["source"]):
+    for f in files:
+        path = os.path.relpath(os.path.join(root, f), ".")
+       	fileHandle(path)
+        lengthOfFile.append(len(wholeData)-cnt)
+        cnt = len(wholeData)
+
+
+def allUnique(x):
+	seen = list()
+	return not any(i in seen or seen.append(i) for i in x)
+
+
+while True:
+	meanVector = random.sample(wholeData, numOfClusters)
+	if allUnique(meanVector):
+		break
+
+
+#Now, k-means clustering
+J = 0.0  # present Cost function
+Jprev = -1.0  # previous Cost function
+threshold = 1e-3
+
+clusters = []
+for i in range(numOfClusters):
+	clusters.append([])
+
+meanVector = np.array(meanVector)
+
+while True:
+    J = assignDataPt()
+    print("J: ",J, " ; Jprev: ",Jprev)
+    if Jprev != -1 and Jprev - J < threshold:
+        break
+    Jprev = J
+    reCalcMean()
