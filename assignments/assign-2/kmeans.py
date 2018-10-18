fc76021ff63cb41260333e37c70f3d198cb13608
Aman Khandelwal <amankh1999@gmail.com>
2018-10-18 16:31:53 +0530

    changed list to numpy array

--- assignments/assign-2/kmeans.py
+++ assignments/assign-2/kmeans.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os, argparse, math, random
-from sklearn.cluster import KMeans
 from datetime import datetime
+from sklearn.cluster import KMeans
 
 ''' 
 	Select k distinct random vectors from the features dataset for each (class(combining all images data))
@@ -10,7 +10,7 @@ from datetime import datetime
 '''
 start_time = datetime.now()
 
-numOfClusters = 32	
+numOfClusters = 32
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--source", required=True, help="Raw data set location")
@@ -25,52 +25,55 @@ def fileHandle(fileName):
 	for line in file:
 		teLine = line.rstrip('\n ').split(' ')
 		nLine = [float(i) for i in teLine]
+		nLine = np.array(nLine)
 		wholeData.append(nLine)
 
 	file.close()
 	return
 
-# def euclidDist(centroid, dataPt):
-# 	dataPt = np.array(dataPt)
-# 	ldist = centroid-dataPt
-# 	return np.sum(np.transpose(ldist)*ldist)
+def euclidDist(centroid, dataPt):
+	# dataPt = np.array(dataPt)
+	ldist = centroid-dataPt
+	return np.sum(np.transpose(ldist)*ldist)
 
 def distArray(dataPt):
 	distVector = np.zeros((numOfClusters))
 	for ind in range(numOfClusters):
-		norm = np.linalg.norm(meanVector[ind] - dataPt)
-		distVector[ind] = norm**2 #euclidDist(meanVector[ind], dataPt)
+		# norm = np.linalg.norm(meanVector[ind] - dataPt)
+		distVector[ind] = euclidDist(meanVector[ind], dataPt)
 	return distVector
 
 #assignment of clusters
 def assignDataPt():
 	costFunc = 0.0
 	for dataPt in wholeData:
+		# print(type(dataPt))
 		distVector = distArray(dataPt)
-		noCl = np.argmin(distVector)
+		noCl = np.argmin(distVector)	
 		clusters[noCl].append(dataPt)
 		costFunc += distVector[noCl]
 	return costFunc
 
-# def findMean(component):
-# 	total = np.zeros((len(component[0])))
-# 	for point in component:
-# 		total += np.array(point)
-# 	total /= len(component)
-# 	return total
+def findMean(component):
+	component = np.array(component)
+	total = np.zeros((len(component[0])))
+	for point in component:
+		total += point
+	total /= len(component)
+	return total
 
 
 def reCalcMean():
 	for i in range(numOfClusters):
 		# print("cluser = ", clusters[i], ": i = ", i)
 		# print("mean by np = ", np.mean(np.array(clusters[i]), axis=0))
-		meanVector[i] = np.mean(np.array(clusters[i]), axis=0)#findMean(clusters[i])
+		meanVector[i] = findMean(clusters[i])
 		clusters[i] = []
 
 
-def allUnique(x):
-	seen = list()
-	return not any(i in seen or seen.append(i) for i in x)
+# def allUnique(x):
+# 	seen = list()
+# 	return not any(i in seen or seen.append(i) for i in x)
 
 
 print("Process start")
@@ -82,40 +85,49 @@ for root, dirs, files in os.walk(args["source"]):
 		lengthOfFile.append(len(wholeData)-cnt)
 		cnt = len(wholeData)
 
-while True:
-	meanVector = random.sample(wholeData, numOfClusters)
-	if allUnique(meanVector):
-		break
 
+
+wholeData = np.array(wholeData)
+meanVector = []
+# while True:
+# 	meanVector = random.sample(wholeData, numOfClusters)
+# 	if allUnique(meanVector):
+# 		break
+
+def initMean():
+	global meanVector
+	temp = random.sample(range(len(wholeData)-1), numOfClusters)
+	for assign in temp:
+		meanVector.append(assign)
+	meanVector = np.array(meanVector)
 
 #Now, k-means clustering
 J = 0.0					#present Cost function
-Jprev = -1.0				#previous Cost function
+Jprev = -1.0			#previous Cost function
 threshold = 1e-3
 
 clusters = []
 for i in range(numOfClusters):
 	clusters.append([])
 
-meanVector = np.array(meanVector)
+# meanVector = np.array(meanVector)
 
-wholeData = np.array(wholeData)
 # kmeans = KMeans(n_clusters=32, init='random', n_init=10, max_iter=300, tol=0.001, precompute_distances='auto', verbose=0, random_state=None, copy_x=True, n_jobs=None, algorithm='full').fit(wholeData)
 # print(kmeans.n_iter_)
 # print(kmeans.inertia_)
 
+initMean()
 
 counter = 0
-loopStarttime = datetime.now()
 while True:
-	print(meanVector)
+	a = datetime.now()
 	J = assignDataPt()
-	print(counter," : J: ", J, "\t : ",(Jprev-J)," : ",(datetime.now()-loopStarttime))
+	# print(counter," : J: ", J, "\t : ",(Jprev-J)," : ",(datetime.now()-loopStarttime))
+	print(counter, " : Cost = ", (Jprev-J))
 	counter += 1
-	if Jprev != -1 and abs(Jprev - J) < threshold:
+	if Jprev != -1 and Jprev - J < threshold:
 		break
 	Jprev = J
 	reCalcMean()
-	loopStarttime = datetime.now()
-
-print("Total time taken ", (datetime.now()-start_time))
+	b = datetime.now()
+	print(b-a)
