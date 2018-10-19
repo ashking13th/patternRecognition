1b722049b4d51fff01d280e3e4212e3714175cff
Amrendra Singh <ashking13th@gmail.com>
2018-10-19 22:32:52 +0530

    pata nhi kya

--- assignments/assign-2/cellProcess.py
+++ assignments/assign-2/cellProcess.py
@@ -1,6 +1,6 @@
-import gmm
+# import gmm
 import os
-import kmeans
+# import kmeans
 import numpy as np
 import grapher
 import argparse
@@ -10,20 +10,10 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--source", required=True, help="Raw data set location")
 ap.add_argument("-d", "--dest", required=True, help="Destination location")
 ap.add_argument("-m", "--mean", required=True, help="mean file location")
-ap.add_argument("-c", "--cov", required=True, help="covaMat file location")
+# ap.add_argument("-c", "--cov", required=True, help="covaMat file location")
 
 args = vars(ap.parse_args())
 
-i = 0
-for root, dirs, files in os.walk(args["source"]):
-    for f in files:
-        i += 1
-        print("Image No. : ", i)
-        path = os.path.relpath(os.path.join(root, f), ".")
-        target = os.path.relpath(os.path.join(root, os.path.splitext(f)[0]))
-        sg.segment(fileHandle(path), target, f)
-
-menaVector = fileHandle(args['mean'])
 def fileHandle(fileName):
     wholeData = []
     file = open(fileName)
@@ -34,3 +24,16 @@ def fileHandle(fileName):
         wholeData.append(nLine)
     file.close()
     return wholeData
+
+
+meanVector = fileHandle(args['mean'])
+print(meanVector)
+i = 0
+for root, dirs, files in os.walk(args["source"]):
+    for f in files:
+        print("File: ",f)
+        i += 1
+        print("Image No. : ", i)
+        path = os.path.relpath(os.path.join(root, f), ".")
+        target = os.path.relpath(os.path.join(root, os.path.splitext(f)[0]))
+        sg.segment(fileHandle(path), target, f, meanVector, args['dest'])
--- assignments/assign-2/segmentation.py
+++ assignments/assign-2/segmentation.py
@@ -4,82 +4,84 @@ import os
 import argparse
 from PIL import Image
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--source", required=True, help="Raw data set location")
-ap.add_argument("-d", "--dest", required=True, help="Output data set location")
-args = vars(ap.parse_args())
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-s", "--source", required=True, help="Raw data set location")
+# ap.add_argument("-d", "--dest", required=True, help="Output data set location")
+# args = vars(ap.parse_args())
 
 
 segmentColors = [[0,0,255],[0,255,0],[255,0,0]]
 colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
 
 def allotCluster(patchFeatures, meanVector):
-    allotment = []
-    clusters = len(meanVector)
-
-    for patch in patchFeatures:
-        dist = []
-        for mean in meanVector:
-            dist.append(np.linalg.norm(patch-mean, axis=1)**2)
-        allotment.append(np.argmin(dist))
-    return allotment
-
-
-def createOverlay(height, breadth, allotment):
-    overlay = []
-
-    for i in range(height):
-        row = []
-        for j in range(breadth):
-            row.append(segmentColors[allotment[int(i/7),int(j/7)]])
-        overlay.append(row)
-    return overlay
-
-
-def process(image, features, meanVector):
-    height = len(image)
-    breadth = len(image[0])
-
-    allotment = []
-    patchCount = 0
-
-    # print("Height: ",height, "; breadth: ",breadth)
-    for i in range(0, height, 7):
-        # print(i,end=' ',flush=True)
-        row = []
-        for j in range(0, breadth, 7):
-            segment = allotCluster(features[patchCount], meanVector)
-            row.append(segment)
-            patchCount += 1
-        allotment.append(row)
+    dist = []
+    for mean in meanVector:
+        dist.append(np.linalg.norm(patchFeatures-mean)**2)
+    mini = np.argmin(dist)
+    return mini
     
-    overlay = createOverlay(height, breadth, allotment)
-    plt.figure()
-
-    plt.subplot(1,2,1)
-    plt.imshow(image, 'gray', interpolation='none')
-    plt.subplot(1,2,2)
-    plt.imshow(image, 'gray', interpolation='none')
-    plt.imshow(overlay, 'jet', interpolation='none', alpha=0.7)
-    plt.show()
 
 
-def cellSegmentaion(height, breadth, allotment, targetPath):
-    img = Image.new('RGB', (512,512), color=(0,0,255)).load()
+# def createOverlay(height, breadth, allotment):
+#     overlay = []
+
+#     for i in range(height):
+#         row = []
+#         for j in range(breadth):
+#             row.append(segmentColors[allotment[int(i/7)][int(j/7)]])
+#         overlay.append(row)
+#     return overlay
+
+
+# def process(image, features, meanVector):
+#     height = len(image)
+#     breadth = len(image[0])
+
+#     allotment = []
+#     patchCount = 0
+
+#     # print("Height: ",height, "; breadth: ",breadth)
+#     for i in range(0, height, 7):
+#         # print(i,end=' ',flush=True)
+#         row = []
+#         for j in range(0, breadth, 7):
+#             segment = allotCluster(features[patchCount], meanVector)
+#             row.append(segment)
+#             patchCount += 1
+#         allotment.append(row)
+    
+#     overlay = createOverlay(height, breadth, allotment)
+#     plt.figure()
+
+#     plt.subplot(1,2,1)
+#     plt.imshow(image, 'gray', interpolation='none')
+#     plt.subplot(1,2,2)
+#     plt.imshow(image, 'gray', interpolation='none')
+#     plt.imshow(overlay, 'jet', interpolation='none', alpha=0.7)
+#     plt.show()
+
+
+def cellSegmentaion(height, breadth, allotment, targetPath, dest, filename):
+    im = Image.new('RGB', (512,512), color=(0,0,255))
+    img = im.load()
     
     for i in range(height):
         for j in range(breadth):
-            img[j,i] = colors[allotment[int(i/7), int(j/7)]]
+            img[j,i] = colors[allotment[int(i/7)][int(j/7)]]
 
-    img.show()
-    img.save(targetPath+".jpg")
+    # img.draw()
+    path = dest+"\\"+filename+".jpg"
+    print("target path: ",path)
+    im.save(path,"JPEG")
+    print("YAAY")
 
-def segment(features, targetPath, filename, meanVector):
+def segment(features, targetPath, filename, meanVector, dest):
     height = 512
     breadth = 512
 
     allotment = []
+    abc = []
     patchCount = 0
 
     # print("Height: ",height, "; breadth: ",breadth)
@@ -87,10 +89,34 @@ def segment(features, targetPath, filename, meanVector):
         # print(i,end=' ',flush=True)
         row = []
         for j in range(0, breadth, 7):
-            segment = allotCluster(features[patchCount], meanVector)
-            row.append(segment)
+            seg = allotCluster(features[patchCount], meanVector)
+            row.append(seg)
+            abc.append(seg)
             patchCount += 1
         allotment.append(row)
 
-    cellSegmentaion(height, breadth, allotment, targetPath)
+    # print(allotment)
+    print("abc len: ",len(abc))
+    xFeature = []
+    yFeature = []
+    numOfClusters = len(meanVector)
+    numOfPoints = len(features)
+    print("Number of points: ",numOfPoints)
+    for i in range(numOfClusters):
+        xFeature.append([])
+        yFeature.append([])
+
+    for i in range(numOfPoints):
+        # print("abc[i]: ",abc[i])
+        # print(yFeature[abc[i]])
+        # print("features[i][0]: ",features[i], "\t ; i: ",i)
+        # print("CAT: ",xFeature[abc[i]])
+        xFeature[abc[i]].append(features[i][0])
+        yFeature[abc[i]].append(features[i][1])
+    
+    for i in range(numOfClusters):
+        plt.scatter(xFeature[i],yFeature[i],marker='.',s=1)
+        plt.scatter([meanVector[i][0]], [meanVector[i][1]], marker='*')
+
+    cellSegmentaion(height, breadth, allotment, targetPath, dest, filename)
     return
